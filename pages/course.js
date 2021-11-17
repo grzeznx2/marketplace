e665db1da68679f90824f12f8bf7edc6b7e47ecf
grzeznx2 <grzeznx2@gmail.com>
2021-11-17 16:05:03 +0100

    add modal

--- pages/course.js
+++ pages/course.js
@@ -1,10 +1,13 @@
-import { CourseHero, Keypoints } from '@components/course'
+import { Modal } from '@components/common'
+import { CourseHero, Curriculum, Keypoints } from '@components/course'
 
 export default function Course() {
   return (
     <div className="relative max-w-7xl mx-auto px-4">
       <CourseHero />
       <Keypoints />
+      <Curriculum />
+      <Modal />
     </div>
   )
 }
