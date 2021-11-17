de5a11050b7c6acf0051774c0aef92315d93ee06
grzeznx2 <grzeznx2@gmail.com>
2021-11-17 18:30:35 +0100

    render each course page dynamically

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,16 +1,41 @@
 import { Modal } from '@components/common'
 import { BaseLayout } from '@components/common/layout'
 import { CourseHero, Curriculum, Keypoints } from '@components/course'
+import { getAllCourses } from '@content/courses/fetcher'
 
-export default function Course() {
+export default function Course({ course }) {
   return (
     <div className="py-4">
-      <CourseHero />
-      <Keypoints />
-      <Curriculum />
+      <CourseHero title={course.title} description={course.description} image={course.coverImage} />
+      <Keypoints points={course.wsl} />
+      <Curriculum locked={true} />
       <Modal />
     </div>
   )
+}
+
+export function getStaticPaths() {
+  const { data } = getAllCourses()
+
+  return {
+    paths: data.map(course => ({
+      params: {
+        slug: course.slug,
+      },
+    })),
+    fallback: false,
+  }
+}
+
+export function getStaticProps({ params }) {
+  const { data } = getAllCourses()
+  const course = data.filter(course => course.slug === params.slug)[0]
+
+  return {
+    props: {
+      course,
+    },
+  }
 }
 
 Course.Layout = BaseLayout
