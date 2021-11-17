03345a306a34a75b402ff67dea8cfdeea098b1a7
grzeznx2 <grzeznx2@gmail.com>
2021-11-17 15:49:10 +0100

    add hero

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Navbar, Footer } from '@components/common'
+import { Navbar, Footer, Hero } from '@components/common'
 
 export default function Home() {
   return (
@@ -6,6 +6,9 @@ export default function Home() {
       <div className="relative bg-white overflow-hidden">
         <div className="relative max-w-7xl mx-auto px-4">
           <Navbar />
+          <div className="fit">
+            <Hero />
+          </div>
         </div>
         <Footer />
       </div>
