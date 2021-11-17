d98372bfd85ff93325b23fecb7e5842da3e17389
grzeznx2 <grzeznx2@gmail.com>
2021-11-17 15:35:21 +0100

    add navbar

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+import { Navbar } from '@components/common'
+
 export default function Home() {
-  return <>Hello</>
+  return (
+    <div>
+      <div className="relative bg-white overflow-hidden">
+        <div className="relative max-w-7xl mx-auto px-4">
+          <Navbar />
+        </div>
+      </div>
+    </div>
+  )
 }
