cdf71db66a986bbe559d1e81aa9f667ec2b449a8
grzeznx2 <grzeznx2@gmail.com>
2021-11-18 18:06:16 +0100

    create marketplace page

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -2,10 +2,13 @@ import { useWeb3 } from '@components/providers'
 import Link from 'next/link'
 import { Button } from '@components/ui/common'
 import { useAccount } from '@components/hooks/web3/useAccount'
+import { useRouter } from 'next/router'
 
-export default function Navbar() {
-  const { connect, isWeb3Loaded, isLoading } = useWeb3()
+export default function Footer() {
+  const { connect, isLoading, isWeb3Loaded } = useWeb3()
   const { account } = useAccount()
+  const { pathname } = useRouter()
+
   return (
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
@@ -15,7 +18,7 @@ export default function Navbar() {
               <Link href="/">
                 <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">Home</a>
               </Link>
-              <Link href="/">
+              <Link href="/marketplace">
                 <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">Marketplace</a>
               </Link>
               <Link href="/">
@@ -27,17 +30,19 @@ export default function Navbar() {
                 <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">Wishlist</a>
               </Link>
               {isLoading ? (
-                <Button disabled={true}>Loading...</Button>
+                <Button disabled={true} onClick={connect}>
+                  Loading...
+                </Button>
               ) : isWeb3Loaded ? (
                 account.data ? (
-                  <Button className="cursor-default" hoverable={false}>
-                    Hi there{account.isAdmin && ' Admin'}
+                  <Button hoverable={false} className="cursor-default">
+                    Hi there {account.isAdmin && 'Admin'}
                   </Button>
                 ) : (
                   <Button onClick={connect}>Connect</Button>
                 )
               ) : (
-                <Button onClick={() => window.open('https://metamask.io/', '_blank')}>
+                <Button onClick={() => window.open('https://metamask.io/download.html', '_blank')}>
                   Install Metamask
                 </Button>
               )}
@@ -45,7 +50,7 @@ export default function Navbar() {
           </div>
         </nav>
       </div>
-      {account.data && (
+      {account.data && !pathname.includes('/marketplace') && (
         <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
           <div className="text-white bg-indigo-600 rounded-md p-2">{account.data}</div>
         </div>
