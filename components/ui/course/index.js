d645d004acd15dda75983223c531897ffa0f958d
grzeznx2 <grzeznx2@gmail.com>
2021-11-19 17:53:21 +0100

    create market pages

--- components/ui/course/index.js
+++ components/ui/course/index.js
@@ -1,4 +1,5 @@
 export { default as CourseCard } from './card'
+export { default as OwnedCourseCard } from './card/OwnedCourseCard'
 export { default as CourseHero } from './hero'
 export { default as CourseList } from './list'
 export { default as Curriculum } from './curriculum'
