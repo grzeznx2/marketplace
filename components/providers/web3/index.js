5d286b4d324e511f5ebf817a9c1b5e3df50e7790
grzeznx2 <grzeznx2@gmail.com>
2021-11-19 14:32:33 +0100

    optimize hooks setup

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -1,4 +1,5 @@
-import { createContext, useContext, useEffect, useMemo, useState } from 'react'
+const { createContext, useContext, useEffect, useState, useMemo } = require('react')
+
 import detectEthereumProvider from '@metamask/detect-provider'
 import Web3 from 'web3'
 import { setupHooks } from './hooks/setupHooks'
@@ -11,22 +12,24 @@ export default function Web3Provider({ children }) {
     web3: null,
     contract: null,
     isLoading: true,
+    hooks: setupHooks(),
   })
 
   useEffect(() => {
     const loadProvider = async () => {
       const provider = await detectEthereumProvider()
-
       if (provider) {
+        const web3 = new Web3(provider)
         setWeb3Api({
           provider,
-          web3: new Web3(provider),
+          web3,
           contract: null,
           isLoading: false,
+          hooks: setupHooks(web3, provider),
         })
       } else {
-        setWeb3Api(web3Api => ({ ...web3Api, isLoading: false }))
-        console.log('Please install MetaMask.')
+        setWeb3Api(api => ({ ...api, isLoading: false }))
+        console.error('Please, install Metamask.')
       }
     }
 
@@ -34,21 +37,19 @@ export default function Web3Provider({ children }) {
   }, [])
 
   const _web3Api = useMemo(() => {
-    const { provider, web3 } = web3Api
-
+    const { web3, provider } = web3Api
     return {
       ...web3Api,
-      isWeb3Loaded: web3 !== null,
-      getHooks: () => setupHooks(web3, provider),
+      isWeb3Loaded: web3 != null,
       connect: provider
         ? async () => {
             try {
-              provider.request({ method: 'eth_requestAccounts' })
-            } catch (error) {
+              await provider.request({ method: 'eth_requestAccounts' })
+            } catch {
               location.reload()
             }
           }
-        : () => {},
+        : () => console.error('Cannot connect to Metamask, try to reload your browser please.'),
     }
   }, [web3Api])
 
@@ -60,7 +61,6 @@ export function useWeb3() {
 }
 
 export function useHooks(cb) {
-  const { getHooks } = useWeb3()
-  const hooks = getHooks()
+  const { hooks } = useWeb3()
   return cb(hooks)
 }
