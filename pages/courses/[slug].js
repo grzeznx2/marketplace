c13d17e296810bfafee526f5e544adf7d21b068f
grzeznx2 <grzeznx2@gmail.com>
2021-11-20 15:30:30 +0100

    display message depending on course state

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,26 +1,51 @@
-import { Modal } from '@components/ui/common'
-import { BaseLayout } from '@components/ui/layout'
-import { CourseHero, Curriculum, Keypoints } from '@components/ui/course'
-import { getAllCourses } from '@content/courses/fetcher'
 import { useAccount, useOwnedCourse } from '@components/hooks/web3'
+import { Message, Modal } from '@components/ui/common'
+import { CourseHero, Curriculum, Keypoints } from '@components/ui/course'
+import { BaseLayout } from '@components/ui/layout'
+import { getAllCourses } from '@content/courses/fetcher'
 
 export default function Course({ course }) {
   const { account } = useAccount()
   const { ownedCourse } = useOwnedCourse(course, account.data)
+  const courseState = ownedCourse.data?.state
+  // const courseState = "deactivated"
 
-  console.log(ownedCourse)
   return (
-    <div className="py-4">
-      <CourseHero
-        hasOwner={!!ownedCourse.data}
-        title={course.title}
-        description={course.description}
-        image={course.coverImage}
-      />
+    <>
+      <div className="py-4">
+        <CourseHero
+          hasOwner={!!ownedCourse.data}
+          title={course.title}
+          description={course.description}
+          image={course.coverImage}
+        />
+      </div>
       <Keypoints points={course.wsl} />
+      {courseState && (
+        <div className="max-w-5xl mx-auto">
+          {courseState === 'purchased' && (
+            <Message type="warning">
+              Course is purchased and waiting for the activation. Process can take up to 24 hours.
+              <i className="block font-normal">
+                In case of any questions, please contact info@eincode.com
+              </i>
+            </Message>
+          )}
+          {courseState === 'activated' && (
+            <Message type="success">Eincode wishes you happy watching of the course.</Message>
+          )}
+          {courseState === 'deactivated' && (
+            <Message type="danger">
+              Course has been deactivated, due the incorrect purchase data. The functionality to
+              watch the course has been temporaly disabled.
+              <i className="block font-normal">Please contact info@eincode.com</i>
+            </Message>
+          )}
+        </div>
+      )}
       <Curriculum locked={true} />
       <Modal />
-    </div>
+    </>
   )
 }
 
@@ -28,9 +53,9 @@ export function getStaticPaths() {
   const { data } = getAllCourses()
 
   return {
-    paths: data.map(course => ({
+    paths: data.map(c => ({
       params: {
-        slug: course.slug,
+        slug: c.slug,
       },
     })),
     fallback: false,
@@ -39,7 +64,7 @@ export function getStaticPaths() {
 
 export function getStaticProps({ params }) {
   const { data } = getAllCourses()
-  const course = data.filter(course => course.slug === params.slug)[0]
+  const course = data.filter(c => c.slug === params.slug)[0]
 
   return {
     props: {
