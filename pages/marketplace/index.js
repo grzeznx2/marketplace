8bb57b85c9191080f7e4eb0b4b846659ee1a00bd
grzeznx2 <grzeznx2@gmail.com>
2021-11-20 12:44:30 +0100

    add logic for purchasing courses

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -6,13 +6,35 @@ import { Button } from '@components/ui/common'
 import { OrderModal } from '@components/ui/order'
 import { useState } from 'react'
 import { MarketHeader } from '@components/ui/marketplace'
+import { useWeb3 } from '@components/providers'
 
 export default function Marketplace({ courses }) {
   const [selectedCourse, setSelectedCourse] = useState(null)
-  const { purchaseAvaiable } = useWalletInfo()
+  const { web3, contract } = useWeb3()
+  const { purchaseAvaiable, account } = useWalletInfo()
 
-  const purchaseCourse = order => {
-    alert(JSON.stringify(order))
+  const purchaseCourse = async order => {
+    const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id)
+
+    const orderHash = web3.utils.soliditySha3(
+      { type: 'bytes16', value: hexCourseId },
+      { type: 'address', value: account.data }
+    )
+
+    const emailHash = web3.utils.sha3(order.email)
+
+    const proof = web3.utils.soliditySha3(
+      { type: 'bytes32', value: emailHash },
+      { type: 'bytes32', value: orderHash }
+    )
+
+    const value = web3.utils.toWei(String(order.price))
+
+    try {
+      await contract.methods.purchaseCourse(hexCourseId, proof).send({ from: account.data, value })
+    } catch (error) {
+      console.log(error.message)
+    }
   }
 
   return (
