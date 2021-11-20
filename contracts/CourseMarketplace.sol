5fde4ee429fc55abd8292e238240f2084f44b879
grzeznx2 <grzeznx2@gmail.com>
2021-11-20 10:09:19 +0100

    start working on contract

---
+++ contracts/CourseMarketplace.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract CourseMarketplace {
+    enum State {
+        Purchased,
+        Activated,
+        Deactivated
+    }
+
+    struct Course {
+        uint256 id;
+        uint256 price;
+        bytes32 proof;
+        address owner;
+        State state;
+    }
+}
